ba907761504fa11fa91cf44e154cddd005bc4d61
Jiangyan Zhao <zhaojy2017@126.com>
2026-05-28 21:08:52 +0800

    fixed: Extract a subset of the data set

--- scripts/main_random_plot.R
+++ scripts/main_random_plot.R
@@ -13,13 +13,13 @@ pdf("figures/rand_perf.pdf", width=9, height=9)
 par(ps = 15, mfrow = c(2, 2), family="serif", mar=c(4,4,3,1))
 
 #-------------- J = 5, phi = 0.2 -----------------------------------
-vioplot(OC_J5p02, col = "grey",
+vioplot(OC_J5p02[1:4], col = "grey",
         ylim = c(-18, 18),
         main = expression((a)*~J == 5*","~phi == 0.2),
         names = c("PCS", "PCA", "above-MTD", "ROD"))
 mtext("Difference (SKBD − Keyboard)", side = 2, line = 2.5)
 abline(h = 0, lty = 2)
-points(1:ncol(OC_J5p02), colMeans(OC_J5p02), 
+points(1:4, colMeans(OC_J5p02[1:4]), 
        pch = 24, cex = 1.1, bg = "white", col = "black")
 legend("topright", inset = 0.02, bty = "n",
        legend = c("Median", "Mean"),
@@ -29,13 +29,13 @@ legend("topright", inset = 0.02, bty = "n",
        pt.cex = c(1.0, 1.1))
 
 #-------------- J = 5, phi = 0.3 -----------------------------------
-vioplot(OC_J5p03, col = "grey",
+vioplot(OC_J5p03[1:4], col = "grey",
         ylim = c(-18, 18),
         main = expression((b)*~J == 5*","~phi == 0.3),
         names = c("PCS", "PCA", "above-MTD", "ROD"))
 mtext("Difference (SKBD − Keyboard)", side = 2, line = 2.5)
 abline(h = 0, lty = 2)
-points(1:ncol(OC_J5p03), colMeans(OC_J5p03), 
+points(1:4, colMeans(OC_J5p03[1:4]), 
        pch = 24, cex = 1.1, bg = "white", col = "black")
 legend("topright", inset = 0.02, bty = "n",
        legend = c("Median", "Mean"),
@@ -45,13 +45,13 @@ legend("topright", inset = 0.02, bty = "n",
        pt.cex = c(1.0, 1.1))
 
 #-------------- J = 6, phi = 0.2 -----------------------------------
-vioplot(OC_J6p02, col = "grey",
+vioplot(OC_J6p02[1:4], col = "grey",
         ylim = c(-18, 18),
         main = expression((c)*~J == 6*","~phi == 0.2),
         names = c("PCS", "PCA", "above-MTD", "ROD"))
 mtext("Difference (SKBD − Keyboard)", side = 2, line = 2.5)
 abline(h = 0, lty = 2)
-points(1:ncol(OC_J6p02), colMeans(OC_J6p02), 
+points(1:4, colMeans(OC_J6p02[1:4]), 
        pch = 24, cex = 1.1, bg = "white", col = "black")
 legend("topright", inset = 0.02, bty = "n",
        legend = c("Median", "Mean"),
@@ -61,13 +61,13 @@ legend("topright", inset = 0.02, bty = "n",
        pt.cex = c(1.0, 1.1))
 
 #-------------- J = 6, phi = 0.3 -----------------------------------
-vioplot(OC_J6p03, col = "grey",
+vioplot(OC_J6p03[1:4], col = "grey",
         ylim = c(-18, 18),
         main = expression((d)*~J == 6*","~phi == 0.3),
         names = c("PCS", "PCA", "above-MTD", "ROD"))
 mtext("Difference (SKBD − Keyboard)", side = 2, line = 2.5)
 abline(h = 0, lty = 2)
-points(1:ncol(OC_J6p03), colMeans(OC_J6p03), 
+points(1:4, colMeans(OC_J6p03[1:4]), 
        pch = 24, cex = 1.1, bg = "white", col = "black")
 legend("topright", inset = 0.02, bty = "n",
        legend = c("Median", "Mean"),
